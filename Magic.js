89cbf99c4f607123f4550975c6d3add6d07e033b
mpeltier25 <mpeltier25@Live.com>
2016-05-14 00:07:35 -0400

    Error checking, removing Fireball since it doesn't like rapid requests
    Error checking, removing Fireball since it doesn't like rapid requests

--- Magic.js
+++ Magic.js
@@ -3,16 +3,16 @@ var request = require("request"),
   fs = require("fs"),
   linereader=require('line-reader');
 var writemagictrollcard=fs.createWriteStream("MagicTrollandToadOutput.csv");
-var writemagicchannelfireballcard=fs.createWriteStream("MagicChannelFireballCardOutput.csv");
+//var writemagicchannelfireballcard=fs.createWriteStream("MagicChannelFireballCardOutput.csv");
 writemagictrollcard.write("Name");
 writemagictrollcard.write(", Troll and Toad card price");
 writemagictrollcard.write(", Total Magic card price");
-writemagicchannelfireballcard.write("Name")
-writemagicchannelfireballcard.write(", Channel Fireball");
-writemagicchannelfireballcard.write(", Total Magic card price");
+//writemagicchannelfireballcard.write("Name")
+//writemagicchannelfireballcard.write(", Channel Fireball");
+//writemagicchannelfireballcard.write(", Total Magic card price");
 var lineNum = 0;
 var TrollandToadPrice=0;
-var ChannelFireballPrice=0;
+//var ChannelFireballPrice=0;
 var columns = [];
 var card = [];
 
@@ -38,6 +38,7 @@ linereader.eachLine('MagicCardsInput.csv', function(line, last){
 	
 function processCard(Card){
 	//Troll and Toad working implementation
+  
   url = "http://www.trollandtoad.com/products/search.php?search_category=&search_words="+Card.CardName;
   
 request(url, function (error, response, body) {
@@ -45,19 +46,23 @@ request(url, function (error, response, body) {
     var $ = cheerio.load(body),
       price = $(".price_text").html();
       
-    console.log("The Card "+Card.CardName+" price is "+ price +" at Troll and Toad");
-	writemagictrollcard.write("\n" + Card.CardName);
-	writemagictrollcard.write("," + price);
-	TrollandToadPrice=TrollandToadPrice+(parseFloat(price.substr(1)));
-	writemagictrollcard.write("," + TrollandToadPrice);
-	 url = "http://store.channelfireball.com/products/search?query="+Card.CardName;
+    
+	if(price != null){
+		console.log("The Card "+Card.CardName+" price is "+ price +" at Troll and Toad");
+		writemagictrollcard.write("\n" + Card.CardName);
+		writemagictrollcard.write("," + price);
+		TrollandToadPrice=TrollandToadPrice+(parseFloat(price.substr(1)));
+		writemagictrollcard.write("," + TrollandToadPrice);
+	}
 	
   } else {
     console.log("We’ve encountered an error: " + error);
   }
 });
-  //Channel Fireball working implementation
-  
+  /*Channel Fireball working implementation, But website is not happy with rapid requests. Makes me sad. Use with Caution and have queries be limited, I have a feeling they like to 
+  * timeout requests.
+  */
+  /*
   url = "http://store.channelfireball.com/products/search?query="+Card.CardName;
   
 request(url, function (error, response, body) {
@@ -67,14 +72,16 @@ request(url, function (error, response, body) {
       
 	console.log("The Card "+Card.CardName+" price is "+ price +" at Channel Fireball");
 	writemagicchannelfireballcard.write("\n" + Card.CardName);
-	writemagicchannelfireballcard.write("," + price);
-	ChannelFireballPrice= ChannelFireballPrice+(parseFloat(price.substr(1)));
-	writemagicchannelfireballcard.write("," + ChannelFireballPrice);
+	if(price != null){
+		writemagicchannelfireballcard.write("," + price);
+		ChannelFireballPrice= ChannelFireballPrice+(parseFloat(price.substr(1)));
+		writemagicchannelfireballcard.write("," + ChannelFireballPrice);
+	}
   } else {
     console.log("We’ve encountered an error: " + error);
   }
 });
-	
+*/	
 }
 
 
